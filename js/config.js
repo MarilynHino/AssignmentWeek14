a2f287bd284cbbe8e71a1a3ccb40e4c65d6b974d
MarilynHino <infotech0410@gmail.com>
2022-11-26 18:41:02 -0600

    modify 2nd js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId:         '',         // e.g. us-east-1_czw50dmc1
-        userPoolClientId:   '',         // e.g. 6oegp8t288gicn8t1ko6amc5vd
-        region:             ''          // e.g. us-east-2
+        userPoolId:         'us-east-1_czw50dmc1',         // e.g. us-east-1_czw50dmc1
+        userPoolClientId:   '6oegp8t288gicn8t1ko6amc5vd',         // e.g. 6oegp8t288gicn8t1ko6amc5vd
+        region:             'us-east-1'          // e.g. us-east-2
     },
     api: {
         invokeUrl:          ''          // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
